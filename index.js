3a4122cd1cb80809283db85b624345b06a1b613f
daeunleeeee <lde7953@gmail.com>
2023-05-17 00:04:08 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+function allowDrop(ev) {
+  ev.preventDefault();
+}
+
+function drag(ev) {
+  ev.dataTransfer.setData("text", ev.target.id);
+}
+
+function drop(ev) {
+  ev.preventDefault();
+  var data = ev.dataTransfer.getData("text");
+  var draggedImage = document.getElementById(data);
+  ev.target.appendChild(draggedImage);
+
+  var opacity = 1;
+  var intervalId = setInterval(function () {
+    if (opacity <= 0) {
+      clearInterval(intervalId);
+      draggedImage.style.display = "none";
+    } else {
+      opacity -= 0.1;
+      draggedImage.style.opacity = opacity;
+    }
+  }, 100);
+}
